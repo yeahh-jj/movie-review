6dc931439399bb27f118553410144933bb21bffb
Yerin <jyll1009@gmail.com>
2025-11-13 19:38:21 +0900

    feat: Genre enum 추가 및 장르 도메인 구성

---
+++ src/main/java/com/yeahhjj/movie_review/domain/Genre.java
@@ -0,0 +1,27 @@
+package com.yeahhjj.movie_review.domain;
+
+public enum Genre {
+    ACTION(1, "액션"),
+    COMEDY(2, "코미디"),
+    DRAMA(3, "드라마"),
+    HORROR(4, "공포"),
+    ROMANCE(5, "로맨스"),
+    SF(6, "SF");
+
+    private final int id;
+    private final String displayName;
+
+    Genre(int id, String displayName) {
+        this.id = id;
+        this.displayName = displayName;
+    }
+
+    public int getGenreId() {
+        return id;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+}
