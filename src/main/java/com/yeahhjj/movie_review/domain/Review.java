4a6dac8e957b703b562292c2920c9f249cfbc503
Yerin <jyll1009@gmail.com>
2025-11-15 04:56:16 +0900

    feat: Review 등록을 위한 입력값 검증을 위해 Review domain에 로직 추가

--- src/main/java/com/yeahhjj/movie_review/domain/Review.java
+++ src/main/java/com/yeahhjj/movie_review/domain/Review.java
@@ -1,4 +1,25 @@
 package com.yeahhjj.movie_review.domain;
 
 public class Review {
+    private final int movieId;
+    private final double rating;
+    private final String content;
+
+    public Review(int movieId, double rating, String content) {
+        this.movieId = movieId;
+        this.rating = rating;
+        this.content = content;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public double getRating() {
+        return rating;
+    }
 }
