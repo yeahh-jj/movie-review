f152da2b1c2b439162442ce015b58af0dacffd2d
Yerin <jyll1009@gmail.com>
2025-11-16 19:53:43 +0900

    feat: 영화 등록 기능 전체 흐름 구현

--- src/main/java/com/yeahhjj/movie_review/controller/Controller.java
+++ src/main/java/com/yeahhjj/movie_review/controller/Controller.java
@@ -1,9 +1,12 @@
 package com.yeahhjj.movie_review.controller;
 
+import com.yeahhjj.movie_review.service.Service;
 import com.yeahhjj.movie_review.view.MovieInputView;
 import com.yeahhjj.movie_review.view.MovieOutputView;
 
 public class Controller {
+    private final Service service = new Service();
+
     public void run() {
         registerMovie();
     }
@@ -11,9 +14,11 @@ public class Controller {
     private void registerMovie() {
         MovieOutputView.printRegisterMovieHeader();
 
-        MovieInputView.inputTitle();
-        MovieInputView.inputDirector();
-        MovieInputView.inputGenreId();
+        String title = MovieInputView.inputTitle();
+        String director = MovieInputView.inputDirector();
+        String genreId = MovieInputView.inputGenreId();
+
+        service.registerMovie(title, genreId, director);
 
         MovieOutputView.printMovieRegistrationSuccess();
     }
